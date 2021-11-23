4933a80dd0ed602089c23c9220f89fc3ed70363d
DuyHo <hoducduy99@gmail.com>
2021-11-23 18:14:42 +0700

    handle Filter days by week in schedule manager

--- src/modules_cb/ScheduleManager/pages/ScheduleManagerPages.tsx
+++ src/modules_cb/ScheduleManager/pages/ScheduleManagerPages.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Avatar, Button, Icon, Input, Popover} from "antd";
+import {Avatar, Button, Icon, Input, Popover, Tooltip} from "antd";
 import {RootState} from "../../../redux/reducers";
 import {connect, ConnectedProps} from "react-redux";
 import {countBookingNumber,} from "../../ProfileManager/redux/actions";
@@ -9,7 +9,7 @@ import ScheduleInterview from "../components/ScheduleInterview";
 import DateBox from "devextreme-react/date-box";
 import DetailScheduleInterview from "../components/DetailScheduleInterview";
 import {getAllSchedule} from "../redux/actions";
-import {DataShowSchedule} from "../types";
+import {DataShowSchedule, ScheduleEntity} from "../types";
 
 const {Search} = Input;
 
@@ -32,14 +32,24 @@ interface IProps extends ReduxProps {
 function ScheduleManagerPages(props: IProps) {
   const dateFormat = 'DD/MM/YYYY';
   const timeFormat = 'HH:mm';
+  const currWeek: string = 'currWeek';
+  const preWeek: string = 'preWeek';
+  const nextWeek: string = 'nextWeek';
 
   useEffect(() => {
     document.title = "Lịch";
     props.countBookingNumber();
     props.getAllSchedule();
+    // if(props.schedule?.result){
+    //   filterDatesByCurrentWeek(props.schedule?.result);
+    // }
   }, []);
 
-  useEffect(()=>{},[])
+  useEffect(() => {
+    if (props.schedule?.result) {
+      filterDatesByWeek(currWeek, props.schedule?.result);
+    }
+  }, [props.schedule?.result])
 
   const [visiblePopover, setVisiblePopover] = useState<boolean>(false);
 
@@ -66,10 +76,6 @@ function ScheduleManagerPages(props: IProps) {
   const [visible, setVisible] = useState(false)
   const [visibleDetail, setVisibleDetail] = useState(false)
   const [idDetail, setIdDetail] = useState<string>('');
-
-  function handleDateChange() {
-    console.log("haha")
-  }
 
   function handlePopupScheduleInterview() {
     setVisible(true)
@@ -104,42 +110,104 @@ function ScheduleManagerPages(props: IProps) {
     const randomColor: string = Math.floor(Math.random() * 16777215).toString(16);
     return "#" + randomColor;
   }
+  const [outObject, setOutObject] = useState<ScheduleEntity | any>([])
 
-  const outObject = props.schedule?.result?.reduce((acc: any, curr: any) => {
-    const queryResult = acc.find((qr: any) => {
-      const a: any = moment(qr.date);
-      const b: any = moment(curr.date);
-      const qrConvert: any = new Date(a)
-      const currConvert: any = new Date(b)
-      qrConvert.setHours(0, 0, 0);
-      currConvert.setHours(0, 0, 0);
-      const diffTime = Math.abs((qrConvert - currConvert) / (1000 * 60 * 60 * 24));
-      return diffTime === 0;
-    });
-    if (queryResult !== undefined) {
-      queryResult.data.push(curr)
-    } else {
-      let newQR = {date: moment(curr.date).format("LL"), data: [curr]};
-      acc.push(newQR);
+
+  function getWeekDates(week: string) {
+    let now = new Date();
+    let getNextWeek = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000);
+    let getPreWeek = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+    let start: any;
+    let end: any;
+    let dayOfWeek :any;
+    let numDay:any;
+
+    switch (week) {
+      case currWeek:
+        start = new Date(now);
+        end = new Date(now);
+         dayOfWeek = now.getDay();
+         numDay = now.getDate();
+        break;
+
+      case preWeek:
+        start = new Date(getPreWeek);
+        end = new Date(getPreWeek);
+        dayOfWeek = getPreWeek.getDay();
+        numDay = getPreWeek.getDate();
+        break;
+
+      case nextWeek:
+        start = new Date(getNextWeek);
+        end = new Date(getNextWeek);
+        dayOfWeek = getNextWeek.getDay();
+        numDay = getNextWeek.getDate();
+        break;
+
+      default:
+        start = new Date(now);
+        end = new Date(now);
+        dayOfWeek = now.getDay();
+        numDay = now.getDate();
     }
-    return acc;
-  }, []);
-  console.log("outObject:", outObject)
+
+    start.setDate(numDay - dayOfWeek + 1);
+    start.setHours(0, 0, 0, 0);
+
+    end.setDate(numDay + (7 - dayOfWeek));
+    end.setHours(0, 0, 0, 0);
+    return [start, end];
+  }
+
+  function filterDatesByWeek(week: string, filterDate: ScheduleEntity[]) {
+    let [start, end] = getWeekDates(week);
+    const datesFilter: any = filterDate?.filter((d: any) => d.date >= +start && d.date < +end);
+    const outObject = datesFilter?.reduce((acc: any, curr: any) => {
+      const queryResult = acc.find((qr: any) => {
+        const a: any = moment(qr.date);
+        const b: any = moment(curr.date);
+        const qrConvert: any = new Date(a)
+        const currConvert: any = new Date(b)
+        qrConvert.setHours(0, 0, 0);
+        currConvert.setHours(0, 0, 0);
+        const diffTime = Math.abs((qrConvert - currConvert) / (1000 * 60 * 60 * 24));
+        return diffTime === 0;
+      });
+      if (queryResult !== undefined) {
+        queryResult.data.push(curr)
+      } else {
+        let newQR = {date: moment(curr.date).format("LL"), data: [curr]};
+        acc.push(newQR);
+      }
+      return acc;
+    }, []);
+    return setOutObject(outObject)
+  }
+
+  function handleWeekClicked(week: string) {
+    filterDatesByWeek(week, props.schedule?.result);
+  }
+
   return (
     <>
       <div className="c-schedule-container">
         <div className="c-schedule-header">
           <div className="c-schedule-header__align-left">
-            <Button style={{fontWeight: 500}}>
+            <Button style={{fontWeight: 500}} onClick={() => handleWeekClicked(currWeek)}>
               <AiOutlineCalendar className="mr-1" size={20}/>
               Tuần này
             </Button>
-            <Button>
-              <GrPrevious/>
-            </Button>
-            <Button>
-              <GrNext/>
-            </Button>
+            <Tooltip placement="top" title="Tuần trước">
+              <Button onClick={() => handleWeekClicked(preWeek)}>
+                <GrPrevious/>
+              </Button>
+            </Tooltip>
+            <Tooltip placement="top" title="Tuần sau">
+              <Button onClick={() => handleWeekClicked(nextWeek)}>
+                <GrNext/>
+              </Button>
+            </Tooltip>
             <div style={{marginRight: 30}} className="align">
               <DateBox defaultValue={moment()} displayFormat="dd/MM/yyyy"
                        type="date"/>
