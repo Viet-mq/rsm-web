e8db630bcaeffa466657eb9a4b348a6783ec5355
DESKTOP-88FAOV3\EdsoLabs <>
2022-01-15 08:18:52 +0700

    Xử lý focus input trong mail

--- src/modules_cb/EmailManager/components/UpdateEmailForm.tsx
+++ src/modules_cb/EmailManager/components/UpdateEmailForm.tsx
@@ -1,9 +1,8 @@
 import {RootState} from "src/redux/reducers";
 import {connect, ConnectedProps} from "react-redux";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import env from "src/configs/env";
-import {ColumnProps} from "antd/lib/table";
-import {Button, Col, Form, Icon, Input, Popconfirm, Row, Tabs} from "antd";
+import {Button, Col, Form, Input, Row, Tabs} from "antd";
 import {deleteJob, getListJob, showFormCreate, showFormUpdate, updateJob} from "../redux/actions";
 import {DeleteJobRequest, JobEntity} from "../types";
 import Search from "antd/es/input/Search";
@@ -56,138 +55,91 @@ function UpdateEmailForm(props: IProps) {
     props.showFormUpdate(true, entity);
   }
 
-  const columns: ColumnProps<JobEntity>[] = [
-    {
-      title: 'STT',
-      key: 'index',
-      width: 40,
-      align: "center",
-      render: (text, record, index) => {
-        return (page - 1) * 10 + index + 1
-      }
-    },
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      width: 100,
-    },
-    {
-      title: () => {
-        return <div style={{whiteSpace: 'nowrap'}}>Thao tác</div>;
-      },
-      dataIndex: 'action',
-      width: 100,
-      fixed: 'right',
-      render: (_text: string, record: JobEntity) => {
-        return (
-          <div style={{whiteSpace: 'nowrap'}}>
-            <Popconfirm
-              title="Bạn muốn xóa Job này chứ ?"
-              okText="Xóa"
-              onCancel={event => {
-                event?.stopPropagation();
-              }}
-              onConfirm={event => handleDelete(event, record)}
-            >
-              <Button
-                size="small"
-                className="ant-btn ml-1 mr-1 ant-btn-sm"
-                onClick={event => {
-                  event.stopPropagation();
-                }}
-              >
-                <Icon type="delete" theme="filled"/>
-              </Button>
-            </Popconfirm>
-            <Button size="small" className="ant-btn ml-1 mr-1 ant-btn-sm"
-                    onClick={event => handleEdit(event, record)}
-            >
-              <Icon type="edit"/>
-            </Button>
-          </div>
-        );
-      },
-    },
-  ];
+  const inputEl = useRef<any>(null);
+  const onButtonClick = () => {
+    // `current` points to the mounted text input element
+    inputEl.current.focus();
 
+  };
 
   return (
     <div className="page-container">
       <Row style={{display: "flex"}}>
         <Col span={18} className='mail-content grid-left'>
-              <div >
-                <Form>
-                  <Form.Item className="form-label" label="Tên mẫu mail" labelCol={{span: 24}}
-                             wrapperCol={{span: 24}}>
-                    {getFieldDecorator('name', {
-                      initialValue: 'Thư xác nhận',
-                      rules: [
-                        {
-                          message: 'Vui lòng nhập tên mẫu',
-                          required: true,
-                        },
-                      ],
-                    })(
-                      <Input placeholder="Nhập tên mẫu" className="bg-white text-black"/>
-                    )}
-                  </Form.Item>
+          <div>
+            <Form>
+              <Form.Item className="form-label" label="Tên mẫu mail" labelCol={{span: 24}}
+                         wrapperCol={{span: 24}}>
+                {getFieldDecorator('name', {
+                  initialValue: 'Thư xác nhận',
+                  rules: [
+                    {
+                      message: 'Vui lòng nhập tên mẫu',
+                      required: true,
+                    },
+                  ],
+                })(
+                  <Input placeholder="Nhập tên mẫu" className="bg-white text-black"/>
+                )}
+              </Form.Item>
 
-                  <Form.Item className="form-label" label="Tiêu đề mail" labelCol={{span: 24}}
-                             wrapperCol={{span: 24}}>
-                    {getFieldDecorator('name', {
-                      initialValue: "Bạn vừa ứng tuyển vào  [ Tên công ty ]",
-                      rules: [
-                        {
-                          message: 'Vui lòng nhập tiêu đề mail',
-                          required: true,
-                        },
-                      ],
-                    })(
-                      <Input placeholder="Nhập tiêu đề" className="bg-white text-black"/>
-                    )}
-                  </Form.Item>
+              <Form.Item className="form-label" label="Tiêu đề mail" labelCol={{span: 24}}
+                         wrapperCol={{span: 24}}>
+                {getFieldDecorator('name', {
+                  initialValue: "Bạn vừa ứng tuyển vào  [ Tên công ty ]",
+                  rules: [
+                    {
+                      message: 'Vui lòng nhập tiêu đề mail',
+                      required: true,
+                    },
+                  ],
+                })(
+                  <Input placeholder="Nhập tiêu đề" className="bg-white text-black"/>
+                )}
+              </Form.Item>
 
 
-                  <Form.Item className="form-label quill-editor" label="Nội dung" labelCol={{span: 24}}
-                             style={formItemHeight} wrapperCol={{span: 24}}>
-                    {getFieldDecorator('jobDescription', {
-                      initialValue: '',
-                      rules: [
-                        {
-                          message: 'Vui lòng nhập nội dung',
-                          required: true,
-                        },
-                        // {
-                        //   validator: validateReactQuill,
-                        // },
-                      ],
-                    })(
-                      // <ReactQuill
-                      //   style={{...fontWeightStyle, ...textEditorHeight}}
-                      //   theme={'snow'}
-                      //   modules={modules}
-                      //   formats={formats}
-                      //   bounds={'.app'}
-                      //   placeholder="Mô tả công việc"
-                      // />
-                      <TextArea  placeholder="Nội dụng" style={{height: 150}}
-                                className="bg-white text-black"/>
-                    )}
-                  </Form.Item>
+              <Form.Item className="form-label quill-editor" label="Nội dung" labelCol={{span: 24}}
+                         style={formItemHeight} wrapperCol={{span: 24}}>
+                {getFieldDecorator('context', {
+                  initialValue: '',
+                  rules: [
+                    {
+                      message: 'Vui lòng nhập nội dung',
+                      required: true,
+                    },
+                    // {
+                    //   validator: validateReactQuill,
+                    // },
+                  ],
+                })(
+                  // <ReactQuill
+                  //   style={{...fontWeightStyle, ...textEditorHeight}}
+                  //   theme={'snow'}
+                  //   modules={modules}
+                  //   formats={formats}
+                  //   bounds={'.app'}
+                  //   placeholder="Mô tả công việc"
+                  // />
+                  <TextArea ref={inputEl} placeholder="Nội dụng" style={{height: 150}}
+                            className="bg-white text-black"/>
+                )}
+              </Form.Item>
 
-                </Form>
-              </div>
+            </Form>
+          </div>
         </Col>
         <Col span={6} className="email-option-variable grid-left">
           <div>
             <div className="form-label mb-3">Biến mẫu</div>
             <div className="mb-2">
-              <span style={{color:"red"}}>[ </span><span>Vị trí tuyển dụng</span><span style={{color:"red"}}> ]</span>
+              <span style={{color: "red"}}>[ </span><span>Vị trí tuyển dụng</span><span style={{color: "red"}}> ]</span>
             </div>
             <div>
-              <span style={{color:"red"}}>[ </span><span>Vị trí tuyển dụng</span><span style={{color:"red"}}> ]</span>
+              <span style={{color: "red"}}>[ </span><span>Vị trí tuyển dụng</span><span style={{color: "red"}}> ]</span>
             </div>
           </div>
+          <Button onClick={onButtonClick}>Haaha</Button>
         </Col>
       </Row>
     </div>
