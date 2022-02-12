afd1f39587913bb6b5273ce7c258ce034167cd8f
DESKTOP-88FAOV3\EdsoLabs <>
2022-02-12 21:48:22 +0700

    thêm giao diện lịch nhắc

--- src/modules_cb/ReminderManager/pages/ReminderManagerPages.tsx
+++ src/modules_cb/ReminderManager/pages/ReminderManagerPages.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {RootState} from "../../../redux/reducers";
 import {connect, ConnectedProps} from "react-redux";
-import 'moment/locale/vi';
-import { Calendar, Views } from 'react-big-calendar'
+// import 'moment/locale/vi';
+import { Calendar, Views,momentLocalizer} from 'react-big-calendar'
 import Events from "../components/Events";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 
 const mapStateToProps = (state: RootState) => ({})
@@ -12,15 +14,14 @@ const connector = connect(mapStateToProps, {});
 
 type ReduxProps = ConnectedProps<typeof connector>;
 
-interface IProps extends ReduxProps {
-
-}
+interface IProps extends ReduxProps {}
 
 function ReminderManagerPages(props: IProps) {
+  const localizer = momentLocalizer(moment);
 
   return (
     <>
-      <div className="c-schedule-container">
+      <div className=" status-cv-container">
         <Calendar
           events={Events}
           step={15}
@@ -29,9 +30,7 @@ function ReminderManagerPages(props: IProps) {
           defaultView={Views.WEEK}
           defaultDate={new Date(2015, 3, 12)}
         />
-
       </div>
-
     </>
   );
 }
