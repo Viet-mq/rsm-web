4b57fc5cc83b6ff8d645262cdb742707e5c36fab
DuyHo <hoducduy99@gmail.com>
2022-02-12 23:07:42 +0700

    dựng giao diện calendar done

--- src/modules_cb/ReminderManager/pages/ReminderManagerPages.tsx
+++ src/modules_cb/ReminderManager/pages/ReminderManagerPages.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import {RootState} from "../../../redux/reducers";
 import {connect, ConnectedProps} from "react-redux";
 // import 'moment/locale/vi';
-import { Calendar, Views,momentLocalizer} from 'react-big-calendar'
+import {Calendar, momentLocalizer, Views} from 'react-big-calendar'
 import Events from "../components/Events";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -14,16 +14,25 @@ const connector = connect(mapStateToProps, {});
 
 type ReduxProps = ConnectedProps<typeof connector>;
 
-interface IProps extends ReduxProps {}
+interface IProps extends ReduxProps {
+}
 
 function ReminderManagerPages(props: IProps) {
   const localizer = momentLocalizer(moment);
-
+  const [eventReminder, setEventReminder] = useState<any>(Events);
+  function handleSelect ({start, end}:any){
+    const title = window.prompt('New Event name')
+    if (title) setEventReminder([...eventReminder, {start, end, title}])
+  }
+  console.log(eventReminder)
   return (
     <>
       <div className=" status-cv-container">
         <Calendar
-          events={Events}
+          selectable
+          onSelectSlot={handleSelect}
+          onSelectEvent={event => alert(event.title)}
+          events={eventReminder}
           step={15}
           timeslots={8}
           localizer={localizer}
